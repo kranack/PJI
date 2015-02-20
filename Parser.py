2f952de68341a733c36b0f6221599e914cc6c11a
Damien Calesse <damien.calesse@gmail.com>
2015-02-20 15:59:15 +0100

    clean

--- Parser.py
+++
@@ -1,8 +0,0 @@
-class Parser:
-
-    def __init__(self, regex):
-        self.regex = regex
-
-    def get_parse_text(self, orgfile):
-
-        return $text
--- test_database.py
+++
@@ -1,9 +0,0 @@
-import Database
-
-test = Database.Database('database.db')
-
-res = test.select('Tasks')
-for row in res:
-    print '#{0}: {1} ajoutee le {2}'.format(row[0], row[1], row[2])
-
-#test.insert('Tasks', [("name", "Test"), ("date_create", "2015-02-17")])
